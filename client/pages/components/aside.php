7538ca54cd8de9608ea48c20e54b456a03274581
Jay-Ubisse <joaquimubisse@gmail.com>
2022-11-15 18:15:05 +0200

    reorganizing the menu

--- client/pages/components/aside.php
+++ client/pages/components/aside.php
@@ -3,19 +3,46 @@
         <strong>FOTO</strong>
     </div>
     <h1 class="text-center text-white font-bold text-lg mt-2 pb-5 border-b border-b-slate-200 w-11/12 mx-auto"><?php echo $userData["name"] ?></h1>
-    <div>
-        <h2 class="md:block hidden text-center text-white font-bold text-lg mt-5 mb-5 hover:text-yellow-800 hover:text-2xl transition-transform"><a href="../home/">Início</a></h2>
-    </div>
-    <div>
-        <h2 class="md:block hidden text-center text-white font-bold text-lg mb-5 hover:text-yellow-800  hover:text-2xl transition-transform"><a href="../my-profile/">Perfil</a></h2>
-    </div>
-    <div>
-        <h2 class="md:block hidden text-center text-white font-bold text-lg mb-5 hover:text-yellow-800  hover:text-2xl transition-transform"><a href="#">Notificações</a></h2>
-    </div>
-    <div>
-        <h1 class="md:block hidden text-center text-white font-bold text-lg mb-5 hover:text-yellow-800  hover:text-2xl transition-transform"><a href="#">Mensagens</a></h1>
-    </div>
-    <div>
-        <h2 class="md:block hidden text-center text-white font-bold text-lg mb-5 hover:text-yellow-800  hover:text-2xl transition-transform"><a href="#">Explorar</a></h2>
-    </div>
+    <table class="w-fit mx-auto mt-6 text-white text-lg">
+        <tr class="hover:text-orange-800 hover:text-xl transition-transform">
+            <td>
+                <span class="h-fit"><i class="fa-solid fa-house"></i></span>
+            </td>
+            <td>
+                <h2 class="font-bold m-4"><a href="../home/">Início</a></h2>
+            </td>
+        </tr>
+        <tr class="hover:text-orange-800 hover:text-xl transition-transform">
+            <td>
+                <span><i class="fa-solid fa-user"></i></span>
+            </td>
+            <td>
+                <h2 class="font-bold m-4"><a href="../my-profile/">Perfil</a></h2>
+            </td>
+        </tr>
+        <tr class="hover:text-orange-800 hover:text-xl transition-transform">
+            <td>
+                <span><i class="fa-solid fa-bell"></i></span>
+            </td>
+            <td>
+                <h2 class="font-bold m-4"><a href="#">Notificações</a></h2>
+            </td>
+        </tr>
+        <tr class="hover:text-orange-800 hover:text-xl transition-transform">
+            <td>
+                <span><i class="fa-solid fa-envelope"></i></span>
+            </td>
+            <td>
+                <h1 class="font-bold m-4"><a href="#">Mensagens</a></h1>
+            </td>
+        </tr>
+        <tr class="hover:text-orange-800 hover:text-xl transition-transform">
+            <td >
+                <span><i class="fa-solid fa-hashtag"></i></span>
+            </td>
+            <td>
+                 <h2 class="font-bold m-4"><a href="#">Explorar</a></h2>
+            </td>
+        </tr>
+    </table>
 </div>
